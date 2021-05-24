0eb53045cbd21c3c12dff8e09b4eac1c4b6d3752
thalesousa <thalestjsb@gmail.com>
2021-05-24 11:03:47 -0300

    Filtro adicionado em filmes

--- src/pages/Filmes/index.tsx
+++ src/pages/Filmes/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import CardList from "../../components/CardList";
 import { Header } from "../../components/Header";
 import { api } from "../../services/api";
-import { Container, Context } from "./styles";
+import { Container, Context, SelectContainer } from "./styles";
 
 type CardInfoProps = {
   name: string;
@@ -10,23 +10,63 @@ type CardInfoProps = {
   thumbnail: string;
 }
 
+type ReleaseProps = {
+  release: number;
+}
+
+type ChronologyProps = {
+  chronology: number;
+}
+
 export default function Filmes() {
   const [filmes, setFilmes] = useState<CardInfoProps[]>([]);
+  const [select, setSelect] = useState('');
 
   useEffect(() => {
     const getValues = async () => {
       const { data } = await api.get(`Filmes`)
-      setFilmes(data);
+      if (!select.trim()){
+        setFilmes(data);
+      }
+
+      switch(select){
+        case 'release':
+          data.sort(function compare(a:ReleaseProps, b:ReleaseProps) {
+            if (a.release < b.release) return -1;
+            if (a.release > b.release) return 1;
+            return 0;
+          });
+          setFilmes(data);
+          break;
+
+        case 'chronology':
+          data.sort(function compare(a:ChronologyProps, b:ChronologyProps) {
+            if (a.chronology < b.chronology) return -1;
+            if (a.chronology > b.chronology) return 1;
+            return 0;
+          });
+          setFilmes(data);
+          break;
+        default:
+          break;
+      }
     }
 
     getValues()
-  }, [])
+  }, [select])
 
   return (
     <>
       <Header />
       <Container>
         <Context>
+          <SelectContainer>
+            <select value={select} onChange={(e) => setSelect(e.target.value)}>
+              <option value="">Filtrar por:</option>
+              <option value="release">Lançamento</option>
+              <option value="chronology">Cronologia</option>
+            </select>
+          </SelectContainer>
           <CardList value={filmes}/>
         </Context>
       </Container>
--- src/pages/Filmes/styles.ts
+++ src/pages/Filmes/styles.ts
@@ -2,13 +2,32 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: calc(100vh - 114px);
-
 `
 
 export const Context = styled.div`
   margin: 0 auto;
   max-width: 1366px;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+`
+
+export const SelectContainer = styled.div`
+  align-self: flex-start;
+  margin-left: 170px;
+  padding-top: 16px;
+
+  select {
+    font-family: "Axiforma Bold";
+    font-size: 16px;
+    color: var(--red);
+    border: 1px solid var(--red);
+    width: 165px;
+    height: 50px;
+    padding: 16px;
+    background-color: #000;
+    border-radius: 10px;
+  }
+
 `
